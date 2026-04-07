40b276a073d7d0a272714fb499c366e515b6e4e3
dxrevo <6ceejey@gmail.com>
2026-04-07 16:06:00 +0000

    classement incorporation

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-65ok_$(xdi%hp+0qv01z#9@%5eihey$a#7g-bn=js!s-md7isd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.30.23", "cleanbase.petrosen.sn"]
 
 
 # Application definition
